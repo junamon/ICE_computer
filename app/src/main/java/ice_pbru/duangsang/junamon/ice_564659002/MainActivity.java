2da1b27fe4a6bc4baf4ab3159252421538cd8579
junamon <fren_efull@hotmail.com>
2016-04-20 13:15:40 +0700

    หน้าแรก

--- app/src/main/java/ice_pbru/duangsang/junamon/ice_564659002/MainActivity.java
+++ app/src/main/java/ice_pbru/duangsang/junamon/ice_564659002/MainActivity.java
@@ -1,13 +1,40 @@
 package ice_pbru.duangsang.junamon.ice_564659002;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bindWidget();
+        buttoncontroller();
+
+    }
+
+    private void buttoncontroller() {
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.facebook.com/fren.efull"));
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void bindWidget() {
+        button = (Button) findViewById(R.id.button);
+
     }
 }
